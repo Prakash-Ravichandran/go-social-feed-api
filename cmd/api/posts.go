cbfc71b003fefc9007eb44e527e9ad742438a9a6
Prakash-Ravichandran <prakashmtech1998@gmail.com>
2026-08-08 13:16:34 +0530

    posts: create a POST in social feed

--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -1,10 +1,15 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/Prakash-Ravichandran/go-social-feed-api/internal/store"
+)
 
 type CreatePostPayload struct {
-	Tilte   string `json:"title"`
-	Content string `json:"content"`
+	Tilte   string   `json:"title"`
+	Content string   `json:"content"`
+	Tags    []string `json:"tags"`
 }
 
 func (app *application) postsHandler(w http.ResponseWriter, r *http.Request) {
@@ -22,10 +27,29 @@ func (app *application) postsHandler(w http.ResponseWriter, r *http.Request) {
 func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request) {
 	var postPayload CreatePostPayload
 
+	// capture user payload info into postpayload
 	if err := ReadJSON(w, http.StatusOK, r, &postPayload); err != nil {
 		WriteErrorJSON(w, http.StatusBadRequest, err.Error())
 		return
 	}
 
-	WriteJSON(w, http.StatusCreated, postPayload)
+	ctx := r.Context()
+
+	posts := &store.Post{
+		Title:   postPayload.Tilte,
+		Content: postPayload.Content,
+		Tags:    postPayload.Tags,
+		// TODO: change after auth
+		UserID: "1",
+	}
+
+	if err := app.store.Posts.Create(ctx, posts); err != nil {
+		WriteErrorJSON(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	if err := WriteJSON(w, http.StatusCreated, posts); err != nil {
+		WriteErrorJSON(w, http.StatusInternalServerError, err.Error())
+		return
+	}
 }
